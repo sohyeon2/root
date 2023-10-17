41eb556468c6b295b1ee3ef3e6733fcf9efbb8c9
sohyeon2 <148218128+sohyeon2@users.noreply.github.com>
2023-10-17 20:43:45 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,75 @@
+package section1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1번 문제
+		//System.out.printf("나는 대학교 %d학년에 재학중입니다.",3);
+		
+		//2번 문제
+		//System.out.printf("%d은 첫 번째, ", 1);
+		//System.out.printf("%f는 두 번째, ", 2.000000);
+		//System.out.print("셋은 세 번째");
+		//System.out.printf("%d은 첫 번째, %f는 두 번째, %s은 세 번째",1, 2.000000,"셋");
+		
+		//3번 문제
+		//System.out.printf("%.1f",1.1);
+		//System.out.println();
+		
+		//System.out.printf("%.2f",1.12);
+		//System.out.println();
+		
+		//System.out.printf("%.3f",1.123);
+		//System.out.println();
+		
+		//System.out.printf("%.4f",1.1234);
+		//System.out.println();
+		
+		//System.out.printf("%.5f",1.12345);
+		//System.out.println();		
+		
+		//System.out.printf("%.6f",1.123456);
+		//System.out.println();	
+		
+		//myage=20을 대입하고 yourage 변수복사=값복사
+		//int myage = 20;
+		//int yourage = myage;
+		
+		//System.out.println(myage);
+		//System.out.print(yourage);
+		
+		//myage에 20대입 yourage 30대입 두개의 변수 교환
+		//int myage = 20;
+		//int yourage = 30;
+		//int temp; 
+		//temp=myage;
+		//myage=yourage;
+		
+		//System.out.println(myage);
+		//System.out.print(temp);
+		
+		//4번 문제
+		//byte a = 20;
+		//short b = 30;
+		//int c = 40;
+		//long d = 50;
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		//System.out.println(d);
+		
+		//byte q = 72;
+		//short w = 71;
+		//int e = 70;
+		//long r = 69;
+		//System.out.println(q);
+		//System.out.println(w);
+		//System.out.println(e);
+		//System.out.println(r);
+		
+		
+
+		}
+
+}
